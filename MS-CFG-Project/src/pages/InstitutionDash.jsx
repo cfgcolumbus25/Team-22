3b3e635fa5541ab9769792f8fe6eb95d40f6bcc4
kashishp695 <kpatel06@terpmail.umd.edu>
2025-11-07 19:40:50 -0500

    Started the institution dashboard page

---
+++ MS-CFG-Project/src/pages/InstitutionDash.jsx
@@ -0,0 +1,25 @@
+export const InstitutionDash = ()=> {
+    return (
+        <div>
+            <div className = "institution-dash">
+                <h1> Last updated: 09/25/25</h1>
+                <table>
+                    <tr> 
+                        <th>Exams:</th>
+                        <th> Minimum Score:</th>
+                        <th>Credits Amount:</th>
+                        <th>Last modified date:</th>
+                        <th>Delete</th>
+                        <th>Update Information</th>
+                    </tr>
+                    <tr>
+                        <td>American Government</td>
+                        <td>52</td>
+                        <td>3.0</td>
+                        <td>12/062024</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
+};
