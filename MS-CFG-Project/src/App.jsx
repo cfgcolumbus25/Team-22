8b5577cbe72669643af11418664ce1655814d1d3
kashishp695 <kpatel06@terpmail.umd.edu>
2025-11-08 00:47:08 -0500

    Kashish

--- MS-CFG-Project/src/App.jsx
+++ MS-CFG-Project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LogInPage from './pages/LogInPage'
 import InstitutionDash from './pages/InstitutionDash'
 import LearnerDash from './pages/LearnerDash'
@@ -12,13 +12,17 @@ function App() {
   return (
       <div>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<LogInPage />} />
-          <Route path="/institution" element={<InstitutionDash />} />
+          <Route path="/institution-dash" element={<InstitutionDash />} />
           <Route path="/learner" element={<LearnerDash />} />
           <Route path="/admin" element={<MSAdminDash />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
     </div>
-  )
+
+)
 }
 
 export default App
+
--- MS-CFG-Project/src/pages/InstitutionDash.jsx
+++ MS-CFG-Project/src/pages/InstitutionDash.jsx
@@ -1,27 +1,42 @@
-export const InstitutionDash = ()=> {
+
+const InstitutionDash = ()=> {
     return(
         <div>
             <div className = "institution-dash">
                 <h1> Last updated: 09/25/25</h1>
-                <table>
+                 <table>
+                  <thead>
                     <tr> 
                         <th>Exams:</th>
                         <th> Minimum Score:</th>
                         <th>Credits Amount:</th>
                         <th>Last modified date:</th>
                         <th>Delete</th>
-                        <th>Update Information</th>
                     </tr>
+                  </thead>
+
+                  <tbody>
                     <tr>
                         <td>American Government</td>
                         <td>52</td>
                         <td>3.0</td>
                         <td>12/06/2024</td>
-                        <td><input type = "checkbox" class = "row-select" aria-label = "Select row"></input></td>
+                        <td><input type = "checkbox" class = "row-select" aria-label = "Select row"/></td>
                     </tr>
+                    <tr>
+                        <td>Biology</td>
+                        <td>49</td>
+                        <td>3.0</td>
+                        <td>12/06/2024</td>
+                        <td><input type = "checkbox" class = "row-select" aria-label = "Select row"/></td>
+                    </tr>
+                    </tbody>
                 </table>
-                </div>
+            </div>
                 <button id = "delete-btn" disabled>Delete Selected</button>
             </div>
     );
 };
+
+export default InstitutionDash
+
